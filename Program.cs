019775d40bd328972ad0fddc4ca0303ec6a4faf8
Kye <promasterboy@outlook.com>
2021-03-30 18:23:38 +1100

    First release

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+namespace Images2Pdf
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "HakuNeko Manga Chapters To PDF // Made by Kye#5000 | https://github.com/kyeondiscord";
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("==========");
+            Console.WriteLine("HakuNeko Manga Chapters To PDF // Made by Kye#5000 | https://github.com/kyeondiscord");
+            Console.WriteLine("==========");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string currentdirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var dirName = new DirectoryInfo(currentdirectory).Name;
+            string[] images = Directory.GetFiles(currentdirectory, "*.jpg*", SearchOption.AllDirectories);
+            List<string> pages = new List<string>(images);
+
+            if (pages.Count == 0)
+            {
+                Console.WriteLine("Put this in the folder HakuNeko generates with all the chapters!");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine($"Generating {dirName}.pdf");
+            Console.Title = $"{dirName} | Initializing...";
+
+            Console.WriteLine($"Found {pages.Count} pages. Press any key to continue...");
+
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine($"Creating {dirName}.pdf ...");
+            PdfDocument document = new PdfDocument();
+            int done = 0;
+            foreach (string filename in pages)
+            {
+                    PdfPage page = document.AddPage();
+                   
+                    // Get an XGraphics object for drawing
+                    XGraphics gfx = XGraphics.FromPdfPage(page);
+                    Bitmap img = new Bitmap(filename);
+                    int imageHeight = img.Height;
+                    int imageWidth = img.Width;
+                    DrawImage(gfx, filename, 0, 0, imageHeight / 2, imageWidth / 2);
+                    done++;
+                    Console.Title = ($"{dirName} | {done}/{pages.Count} Pages done!");
+                    
+            }
+            document.Save($"{dirName}.pdf");
+            Console.WriteLine($"Exported to {dirName}");
+            Console.ReadLine();
+        }
+        public static void DrawImage(XGraphics gfx, string image, int x, int y, int height, int width)
+        {
+            gfx.DrawImage(XImage.FromFile(image), x, y, width, height);
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HakuNekoManga2Pdf")]
+[assembly: AssemblyDescription("Made by Kye#5000")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HakuNekoManga2Pdf")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("76d62503-e99c-4f29-a3e5-24545765d267")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
